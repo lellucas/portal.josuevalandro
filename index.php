482257214e949d8e0540636a5eaa39967cb30d2a
lellucas <contatolellucas@gmail.com>
2024-09-02 17:03:53 -0300

    Update 0.1

---
+++ index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portal Josué Valandro Jr.</title>
+    <link href="https://unpkg.com/matcha-core@1.1.11/matcha-core.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,700&display=swap" rel="stylesheet">
+
+    <style>
+        :root{
+            color-scheme: light dark;
+        }
+        html,body{
+            font-family: "Inter", sans-serif;
+
+        }
+        body.dark-theme {
+            color-scheme: dark;
+        }
+        body.light-theme {
+            color-scheme: light;
+        }
+        .container {
+            max-width: 1200px;
+            margin: 0px auto;
+        }
+    </style>
+  </head>
+  <body class="light-theme">
+    
+    <header class="d-flex">
+        <div>logo</div>
+        <nav>
+        <ul class="text-uppercase">
+            <li>mensagens</li>
+            <li>igreja</li>
+            <li>sobre mim</li>
+            <li><a href="https://loja.josuevalandro.com.br" target="_blank">loja</a></li>
+            <li></li>
+        </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="container">
+            <h1 class="fs-48">
+                Lorem ipsum dolor<br>sit adipiscing elit Ut.           
+            </h1>
+            <div class="w-100-p h-500 radius-16" style="margin:0 auto ;background: linear-gradient(360deg, rgba(0,0,0,1)30%,rgba(0,0,0,0)), url(https://picsum.photos/1216/550) no-repeat center center/cover">
+
+            </div>
+            <div>
+                <span>
+                    <
+                </span>
+                <span>
+                    >
+                </span>
+            </div>
+        </section>
+    </main>
+    <footer>
+        rodapé &copy;
+    </footer>
+  </body>
+</html>
